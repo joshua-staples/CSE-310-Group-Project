08fd663adc2b92cb6478af638043da889173644f
Reecepoulsen <dreecepoulsen@gmail.com>
2021-10-20 21:05:43 -0600

    optimized and commented canvasThreaded. Started implementing it into a model for hw_data.

--- hw_session/models.py
+++ hw_session/models.py
@@ -3,36 +3,32 @@ import json
 from canvasapi import Canvas
 import threading
 
-
 # Create your models here.
-class Tasks (models):
+class hw_data(models):
+    name = models.CharField(max_length=30)
+    due_date = models.DateTimeField()
+    course = models.CharField(max_length=30)
+    completed = models.BooleanField()
 
-    def __init__():
-        pass
-
-    def getTasks():
-        tokenJSON = {}
-        with open('accessToken.json') as file:
-            tokenJSON = json.load(file)
-
-        API_URL = "https://byui.instructure.com"
-        API_KEY = tokenJSON["token"]
+    def get_user_info(self):
+        with open("./static/accessToken.json") as file:
+            userData = json.load(file)
+        API_URL = "https://byui.instruture.com"
+        API_KEY = userData["token"]
         canvas = Canvas(API_URL, API_KEY)
-        myUserID = 167103
+        userID = userData['user_id']
 
-        user = canvas.get_user(myUserID)
-        print(user)
+        return canvas.get_user(userID)
 
-        courses = user.get_courses()
-
-        self.printAssignments(courses)
-
-    def printAssignments(courses):
+    def get_all_assignments(self):
         courseThreads = []
         results = {}
         lock = threading.Lock()
+        user = self.get_user_info()
+        courses = user.get_courses(enrollment_state="active")
+
         for course in courses:
-            thread = threading.Thread(target=getCourseAssignments, args=(course, results, lock))
+            thread = threading.Thread(target=self.get_assignments, args=(course, results, lock))
             courseThreads.append(thread)
 
         for thread in courseThreads:
@@ -40,21 +36,32 @@ class Tasks (models):
 
         for thread in courseThreads:
             thread.join()
+        
+        return results
 
-        # print(results)
-
-        for course in courses:
-            if results[course.id]['assignments'] != []:
-                print(results[course.id])
-
-    def getCourseAssignments(course, results, lock):
-
+    def get_assignments(self, course, results, lock):
+        assignmentList = []
         results[course.id] = {
-        "courseName": '',
-        "assignments": []
+            "courseName": '',
+            "assignments": []
         }
         assignments = course.get_assignments(bucket='upcoming')
         for assignment in assignments:
-            with lock:
-                results[course.id]['courseName'] = course.name
-                results[course.id]['assignments'].append(assignment.name)
+            assignmentList.append({
+                "name" : assignment.name,
+                "due_date" : assignment.due_at,
+                "course" : course.name,
+                "course_id" : course.id,
+                "submitted" : assignment.has_submitted_submissions
+            })
+        
+        with lock:
+            results[course.id]['courseName'] = course.name
+            results[course.id]['assignments'] = list(filter(lambda a: a['submitted'] == False, assignmentList))
+
+
+class session_data(models):
+    goal = models.CharField()
+    time_limit = models.FloatField()
+    selected_assignments = models.JSONField()
+    start_time = models.DateTimeField()
